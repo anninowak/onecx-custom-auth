0102ffcc3a45b9dc3c95bb25c3f878ff10b8c4b3
Annika Nowak <annika.nowak@capgemini.com>
2024-04-23 10:56:00 +0200

    feat: temporarily add authservice and keycloak auth service

---
+++ onecx-custom-auth/libs/my-custom-auth/src/lib/auth.service.ts
@@ -0,0 +1,13 @@
+import { ConfigurationService } from '@onecx/angular-integration-interface';
+
+export interface AuthService {
+  init(): Promise<boolean>;
+
+  getHeaderValues(): Record<string, string>;
+
+  logout(): void;
+}
+
+export type AuthServiceFactory = (params: {
+  configService: ConfigurationService;
+}) => AuthService;
