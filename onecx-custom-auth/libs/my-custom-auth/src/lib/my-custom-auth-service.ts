8bcc212ee16899ccb7f39668ec1a08ba566a2857
Annika Nowak <annika.nowak@capgemini.com>
2024-07-22 12:50:08 +0200

    feat: add sense making console.log messages

--- onecx-custom-auth/libs/my-custom-auth/src/lib/my-custom-auth-service.ts
+++ onecx-custom-auth/libs/my-custom-auth/src/lib/my-custom-auth-service.ts
@@ -1,11 +1,47 @@
 import { ConfigurationService } from '@onecx/angular-integration-interface';
-import { AuthService } from '@onecx/angular-auth;
+import { AuthService } from './auth.service';
+import { KeycloakAuthService } from './keycloak-auth.service';
+import { KeycloakConfig } from 'keycloak-js';
+import { KeycloakService } from 'keycloak-angular';
 
-export class MyCustomAuthService implements AuthService {
+export class MyCustomAuthService
+  extends KeycloakAuthService
+  implements AuthService
+{
+  constructor(configService: ConfigurationService) {
+    super(new KeycloakService(), configService);
+  }
+  override async init(): Promise<boolean> {
+    console.log('MyCustomAuthService: start init');
+    const value = await super.init();
+    console.log('MyCustomAuthService: initResult', value);
+    return value;
+  }
+  override getHeaderValues(): Record<string, string> {
+    const headerValues = super.getHeaderValues();
+    console.log(
+      'HeaderValues: ' +
+        headerValues +
+        'This message derives from the MyCustomAuthLibrary'
+    );
+    return headerValues;
+  }
+  override logout(): void {
+    console.log('Logout: This message derives from the MyCustomAuthLibrary');
+    super.logout();
+  }
 
-    constructor(configService: ConfigurationService) {}
+  override getValidKCConfig(): KeycloakConfig {
+    const config = super.getValidKCConfig();
+    console.log(
+      'Config: ' + config + 'This message derives from the MyCustomAuthLibrary'
+    );
+    return config;
+  }
 }
 
-export default function(params: { configService: ConfigurationService }): AuthService {
-    return new MyCustomAuthService(params.configService);
-  }
+export default function (params: {
+  configService: ConfigurationService;
+}): AuthService {
+  return new MyCustomAuthService(params.configService);
+}
